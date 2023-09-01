10fdfe52c2ef22e400f7c0f96f5ac0dd21ba17ca
SmallMistake <piersquaredup@gmail.com>
2023-08-31 22:24:14 -0500

    Initial Commit.
    Working Customers and Shop vs Microgame Areas.

---
+++ Assets/Scripts/Customers/Customer.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+[CreateAssetMenu(fileName = "Customer", menuName = "ScriptableObjects/Customer", order = 1)]
+public class Customer : ScriptableObject
+{
+    public string customerName;
+    public string gameType;
+    public List<Sprite> sprites;
+    public List<string> microGameScenePaths;
+}
